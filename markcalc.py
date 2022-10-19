212e1888b842b4861e7f5e903adc4c8140c84460
aoifeokh <aoifeokh99@gmail.com>
2022-10-19 16:41:01 +0100

    Adding mark calculator python script

---
+++ markcalc.py
@@ -0,0 +1,9 @@
+englishscore = int(input("Enter your English score: "))
+mathsscore = int(input("Enter your maths score: "))
+sciencescore= int(input("Enter your science score: "))
+
+total = englishscore + mathsscore + sciencescore
+
+avgscore = total/3
+
+print("Your average score is: " + str(avgscore) + ".")
