336a27318754dee4b0c03d3404903d9ceb049fd2
aoifeokh <aoifeokh99@gmail.com>
2022-10-20 10:06:13 +0100

    Adding conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -1,7 +1,24 @@
 mark = int(input("Enter your mark: "))
 
-if mark >= 65:
+#no else
+"""if mark >= 65:
     print("Pass")
 
 if mark < 65:
+    print("Fail")"""
+
+#with else
+"""if mark >= 65:
+    print("Pass")
+else:
+    print("Fail")"""
+
+#multiple boundaries
+if mark >= 87:
+    print("Distinction")
+elif mark >= 75:
+    print("Merit")
+elif mark >= 60:
+    print("Pass")
+else mark < 60:
     print("Fail")
